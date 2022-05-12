20c0d09ab41c920d3857cdfadb59a22d14984299
HarveyQi <HarveyQi7@163.com>
2022-05-12 17:07:21 +0800

    update index path

--- index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./lib/constants/common.type";
-export * from "./lib/constants/fiscal.type";
-export * from "./lib/constants/fprinter.type";
-export * from "./lib/printer/epson/epson-cgi";
---
+++ lib/index.ts
@@ -0,0 +1,4 @@
+export * from "./constants/common.type";
+export * from "./constants/fiscal.type";
+export * from "./constants/fprinter.type";
+export * from "./printer/epson/epson-cgi";
